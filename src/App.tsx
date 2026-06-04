d331d3bfdbfbaed52d6c3596e4fe54a6b5d94d3e
itzmitto <andre.babirian2004@gmail.com>
2026-06-04 13:09:28 +0200

    heb de navbar gemaakt en styling aangepast

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import './index.css';
+import './index2.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <div className="logo">Comicbooks.com</div>
+
+          <ul className="nav-links">
+            <li><a href="#">Types</a></li>
+            <li><a href="#">Genres</a></li>
+            <li><a href="#">Newest</a></li>
+            <li><a href="#">Updated</a></li>
+            <li><a href="#">Added</a></li>
+            <li><a href="#">Random</a></li>
+          </ul>
+        </div>
+
+        <div className="navbar-center">
+          <input
+            type="text"
+            placeholder="Search manga..."
+            className="search-bar"
+          />
+        </div>
+
+        <div className="navbar-right">
+          <button className="filter-btn">Filter</button>
+          <button className="login-btn">Login</button>
+        </div>
+      </nav>
+
+      <main className="content">
+        <h1>Hello World</h1>
+      </main>
     </div>
   );
 };
